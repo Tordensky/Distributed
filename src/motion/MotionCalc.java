14507fc0ebd5ea5c6d412a40dce2cabffa35f588
Simon Nistad <simon.nistad@hotmail.com>
2013-03-05 14:21:57 +0100

    Finished with the Code
    - Works at this point
    - Need to refactor the code

--- src/motion/MotionCalc.java
+++ src/motion/MotionCalc.java
@@ -1,9 +1,17 @@
 package motion;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.regex.*;
 import java.util.Iterator;
 
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -12,6 +20,7 @@ import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.JobPriority;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
@@ -31,22 +40,74 @@ public class MotionCalc {
 				Reporter reporter) throws IOException {
 			
 //			Gets filename
-			String inputLine = key.toString();
-			String[] parsed = inputLine.split("/");
+			String filePath = key.toString();
 			
-			String filenameCurr = parsed[parsed.length - 1]; 
-			String filenamePrev = parsed[parsed.length - 1];
+			String[] parsed = filePath.split("/");
+			
+			String currFilename = parsed[parsed.length - 1];  		
+			String prevFilename = generatePrevFileName(currFilename);
 			
 			String camNum = parsed[parsed.length - 2];
-			String frameNumStr = parseFrameNum(filenameCurr); 
+			
+			String path = filePath.split("/(?!.*/.*)")[0]; 
+					//getPathFromParsed(parsed);
+			
+			String frameNumStr = parseFrameNum(currFilename); 
 			
 			outKey.set(frameNumStr);
 					
-			outValue.set(camNum + "/" + filenameCurr);
+			double FrameValue = AutoProduce(path, currFilename, prevFilename);
+			
+			String resStr = camNum + "/" + FrameValue;
+			outValue.set(resStr);
+			
 			output.collect(outKey, outValue);	
 			
-			outValue.set(camNum + "/" + filenamePrev);
-			output.collect(outKey, outValue);
+		}
+		
+		private String getPathFromParsed(String[] filePathList){
+			String path = null;
+			for (int i = 0; i < filePathList.length - 1; i++) {
+				path += filePathList[i];
+				path += "/";
+			}
+			return path;
+		} 
+		
+		private double AutoProduce(String path, String currFilename, String prevFilename) throws IOException{
+			
+			Path pathCurr = new Path(path + "/" + currFilename);
+			Path pathPrev = new Path(path + "/" + prevFilename);
+			
+			FileSystem fs = FileSystem.get(new Configuration());
+					
+			ImageInputStream currFrameFile = ImageIO.createImageInputStream(fs.open(pathCurr));
+			ImageInputStream prevFrameFile = ImageIO.createImageInputStream(fs.open(pathPrev));
+			
+			BufferedImage prevFrame = ImageIO.read(currFrameFile); 
+			BufferedImage currFrame = ImageIO.read(prevFrameFile);
+			
+			Color prevFrameColor, currFrameColor;
+			
+			int height = -1;
+			int width = -1;
+			
+			if (height <0) { height = currFrame.getHeight(); }
+			if (width <0) { width = currFrame.getWidth(); }
+			
+			int motionSum = 0;
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+					prevFrameColor = new Color(prevFrame.getRGB(x, y));
+					currFrameColor = new Color(currFrame.getRGB(x, y));
+					
+					motionSum += Math.abs(prevFrameColor.getRed() - currFrameColor.getRed());
+					motionSum += Math.abs(prevFrameColor.getRed() - currFrameColor.getRed());
+					motionSum += Math.abs(prevFrameColor.getRed() - currFrameColor.getRed());
+				}
+			}
+			double motionEstimate = motionSum / ((double) height*width);
+			return motionEstimate;
 		}
 		
 		private String parseFrameNum(String fileName){
@@ -63,18 +124,56 @@ public class MotionCalc {
 			}
 			return frameNum;
 		}
+		
+		private String parseFrameNumFromPath(String filePath){
+			String re1=".*?";	// Non-greedy match on filler
+		    String re2="((?:[a-z][a-z\\.\\d\\-]+)\\.(?:[a-z][a-z\\-]+))(?![\\w\\.])";	// Integer Number 1		
+			Pattern pattern = Pattern.compile(re1+re2,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+			Matcher matcher = pattern.matcher(filePath); 
+								
+			String frameNum = null;
+			
+			if (matcher.find())
+			{
+				frameNum = matcher.group(1);
+			}
+			return frameNum;
+		}
+		
+		private String generatePrevFileName(String fileName){
+			int fileNumber = Integer.parseInt(parseFrameNum(fileName));
+			if (fileNumber != 0){
+				fileNumber -= 1;
+			}
+			String result = "frame" + String.format("%04d", fileNumber) + ".jpeg";
+			return result;
+		}
 	}
 	
 	public static class ReduceClass extends MapReduceBase implements Reducer<Text, Text, Text, Text>
 	{
 		@Override
 		public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-			Text value = new Text();
-			while(values.hasNext()){
-				Text tmpValue = values.next();
-				value.append(tmpValue.getBytes(), 0, tmpValue.getLength());
+			
+			double action = 0.0;
+			
+			String cam = "cam1";
+			
+			double nextAction = 0.0;
+			
+			while(values.hasNext()){					
+				String res = values.next().toString();
+				String[] splitRes = res.split("/");
+
+				nextAction = Double.parseDouble(splitRes[1]);
+				
+					if (nextAction > action){
+						action = nextAction;
+						
+						cam = splitRes[0];
+					}	
 			}
-			output.collect(key, value);
+			output.collect(key, new Text(cam));
 		}
 	}
 	
@@ -84,15 +183,13 @@ public class MotionCalc {
 	 */
 	public static void main(String[] args) throws Exception {
 		JobConf conf = new JobConf(MotionCalc.class);
-		conf.setJobName("helloworld");
+		conf.setJobName("HappyCamperOnTheRocks");
 		
 		conf.setOutputKeyClass(Text.class);
 		
 		conf.setOutputValueClass(Text.class);
 		
 		conf.setMapperClass(MapClass.class);
-		
-		conf.setCombinerClass(ReduceClass.class);
 		
 		conf.setReducerClass(ReduceClass.class);
 		
@@ -106,6 +203,7 @@ public class MotionCalc {
 		FileOutputFormat.setOutputPath(conf, new Path(args[2]));
 		// conf.setOutputPath(new Path(args[2]));
 		
+		//conf.setJobPriority(JobPriority.VERY_HIGH);
 		JobClient.runJob(conf);
 	}
 
